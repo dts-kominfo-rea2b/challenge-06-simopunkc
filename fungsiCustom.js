1055324e8bc5d2acfc8aade9d23514457690613c
simopunkc <pembela.allah@gmail.com>
2022-06-08 07:50:03 +0700

    submit challenge 6 oleh budiyanto

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const createPromise = (file, format) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (error, data) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(format(data))
+    })
+  })
+}
+
+const format1 = (data) => {
+  return JSON.parse(data).message.split(" ")[1]
+}
+
+const format2 = (data) => {
+  return JSON.parse(data)[0].message.split(" ")[1]
+}
+
+const format3 = (data) => {
+  return JSON.parse(data)[0].data.message.split(" ")[1]
+}
+
+const bacaData = async (callback) => {
+  try {
+    let result = await Promise.all([
+      createPromise(file1, format1),
+      createPromise(file2, format2),
+      createPromise(file3, format3)
+    ]).then((v) => v)
+    return callback(null, result)
+  } catch (err) {
+    return callback(err, null)
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
